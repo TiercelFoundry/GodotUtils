3d37f2f9a0adde3c8fd7246955bf91efd36931a8
Grahame Watt <grahame@eyrus.com>
2024-05-14 14:42:22 -0700

    vector conversions

--- GodotUtils/VectorExtensions.cs
+++ GodotUtils/VectorExtensions.cs
@@ -28,4 +28,24 @@ public static class VectorExtensions
     {
         return new Vector3(vector.Z, vector.Y, -vector.X);
     }
+
+    public static Vector3 ToVector3(this Vector3I vector)
+    {
+        return new Vector3(vector.X, vector.Y, vector.Z);
+    }
+
+    public static Vector3I Vector3I(this Vector3 vector)
+    {
+        return new Vector3I((int)vector.X, (int)vector.Y, (int)vector.Z);
+    }
+
+    public static Vector2 ToVector2(this Vector2I vector)
+    {
+        return new Vector2(vector.X, vector.Y);
+    }
+
+    public static Vector2I Vector2I(this Vector2 vector)
+    {
+        return new Vector2I((int)vector.X, (int)vector.Y);
+    }
 }
