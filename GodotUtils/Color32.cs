d6cb543ac59272b67042772d236247bd1df08fb8
Grahame Watt <grahame@eyrus.com>
2024-05-14 10:48:46 -0700

    color32

---
+++ GodotUtils/Color32.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace TiercelFoundry.GodotUtils;
+
+[DebuggerDisplay("({R}, {G}, {B}, {A})")]
+public struct Color32
+{
+    public byte R;
+    public byte G;
+    public byte B;
+    public byte A;
+
+    public Color32(byte r, byte g, byte b, byte a)
+    {
+        R = r; G = g; B = b; A = a;
+    }
+
+    public readonly byte[] ToByteArray()
+    {
+        return new byte[] { R, G, B, A };
+    }
+}
---
+++ GodotUtils/Color32Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace TiercelFoundry.GodotUtils;
+
+public static class Color32Extensions
+{
+    public static byte[] ToByteArray(this IList<Color32> colors)
+    {
+        var arr = new byte[4 * colors.Count];
+        for (int i = 0, j = 0; i < colors.Count; i++, j += 4)
+        {
+            arr[j] = colors[i].R;
+            arr[j + 1] = colors[i].G;
+            arr[j + 2] = colors[i].B;
+            arr[j + 3] = colors[i].A;
+        }
+
+        return arr;
+    }
+}
